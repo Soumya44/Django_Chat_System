52b3fb0a7594d0fc519d27da084b1c460b2ad28a
Soumya <soumyaranjanb02@gmail.com>
2018-02-11 20:18:50 +0530

    up

--- chat_bot/views.py
+++ chat_bot/views.py
@@ -15,12 +15,12 @@ def create_msg(request):
         if msg == "":
             return HttpResponse(status=204)
         Message.objects.create(content=request.POST.get('message'))
-        return HttpResponseRedirect('ok')
+        return HttpResponse('ok')
 
 
 def reset_session(request):
     Message.objects.all().delete()
-    return HttpResponseRedirect('ok')
+    return HttpResponse('ok')
 
 
 
